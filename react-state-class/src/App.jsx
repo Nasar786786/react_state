9cadd7b81cec5648a8649345f6b72d072ae0447f
Md Abunasar <abunasarsms040798@gmail.com>
2024-07-29 09:56:29 +0530

    random Num gen

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
+import Lottery from  "./Lottery";
+import { sum } from "./helper";
+
 
 function App() {
+
+     let winCondition = (ticket) => {
+      return ticket[0] === 0;
+     };
+
    return ( 
       <>
-      < TodoList  />
+      <Lottery n={3} winCondition={winCondition} />
+
    </>
 
    );
